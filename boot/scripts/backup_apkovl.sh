9a09b053ef1c7a96cc744f234988678b9169d998
Jack O'Sullivan <jackos1998@gmail.com>
2021-06-30 13:03:06 +0100

    boot: Avoid duplicating backups

--- boot/scripts/backup_apkovl.sh
+++ boot/scripts/backup_apkovl.sh
@@ -7,11 +7,25 @@ RECIPIENT="internet@csc.tcd.ie"
 PATTERN="*.*.tar.*"
 
 echo "Starting backup..."
-find "$BACKUP" -name "$PATTERN" -exec rm {} \;
+
+for path in $(find "$BACKUP" -name "$PATTERN"); do
+	f="$(basename $path | sed 's/\.gpg//')"
+	if [ -z $(find "$LIVE" -name "$f") ]; then
+		echo "Deleting old backup $f.gpg"
+		rm "$path"
+	fi
+done
 
 for path in $(find "$LIVE" -name "$PATTERN"); do
 	f="$(basename $path)"
+
+	if [ -f "$BACKUP/$f.gpg" ] && gpg -q --decrypt "$BACKUP/$f.gpg" 2>/dev/null | cmp --silent "$path"; then
+		echo "$f is up to date, skipping..."
+		continue
+	fi
+
 	echo "Encrypting and backing up $f"
+	rm -f "$BACKUP/$f.gpg"
 
 	gpg -o "$BACKUP/$f.gpg" -r "$RECIPIENT" --sign --encrypt "$path"
 done
